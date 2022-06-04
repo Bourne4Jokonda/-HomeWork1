82e0cf98b8822db437a5b047985f2d9f6bf58824
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-04 07:26:27 +0300

    add max number

---
+++ HomeWork/MaxNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Чтобы узнать наибольшее число, введите 3 числа ");
+Console.WriteLine("Сейчас первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Сейчас второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Сейчас третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("Наибольшее число это - ");
+Console.WriteLine(max);
