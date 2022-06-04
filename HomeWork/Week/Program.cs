ad6a279c9d46778515f802d5a9aaaea42efc0d2d
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-04 07:08:11 +0300

    add week

---
+++ HomeWork/Week/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Чтобы узнать день недели,введите число от 1 до 7 ");
+string numberofday = Console.ReadLine();
+
+if(numberofday == "1")
+{
+    Console.WriteLine("Это понедельник");
+}
+if(numberofday == "2")
+{
+    Console.WriteLine("Это вторник");
+}
+if(numberofday == "3")
+{
+    Console.WriteLine("Это среда");
+}
+if(numberofday == "4")
+{
+    Console.WriteLine("Это четверг");
+}
+if(numberofday == "5")
+{
+    Console.WriteLine("Это пятница");
+}
+if(numberofday == "6")
+{
+    Console.WriteLine("Это суббота");
+}
+if(numberofday == "7")
+{
+    Console.WriteLine("Это воскресенье");
+}
