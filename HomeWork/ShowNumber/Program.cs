d95a44e8176eedba35d595e41ccfe072c965340c
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-08 10:36:59 +0300

    add show number

---
+++ HomeWork/ShowNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Введите число ");
+int A = int.Parse(Console.ReadLine());
+int simple = 1;
+
+simple = ( A % 10);
+Console.WriteLine(simple);
