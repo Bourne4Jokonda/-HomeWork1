b7709bacc6380f55b34047a44e6453a7ba3e32bb
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-08 10:49:28 +0300

    add show number.1

---
+++ HomeWork/ShowNumber.1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Write("Введите число ");
+int A = int.Parse(Console.ReadLine());
+int div = A / 10;
+int simple = 1;
+
+simple = div % 10;
+Console.WriteLine(simple);
