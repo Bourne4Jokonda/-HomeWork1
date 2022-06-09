06e04bbdb4ba716d8848a2cf9e6b637e955027d6
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-09 08:59:01 +0300

    add delete number

---
+++ HomeWork/DeleteNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Удаляем вторую цифру трехзначного числа и выводим двухзначное
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int A1 = 0;
+int A3 = 0;
+int div1 = number % 10;
+int div3 = (number / 100) % 10;
+int sum = 0;
+
+A1 = div1;
+A3 = div3;
+sum = (A3 * 10) + A1;
+
+Console.WriteLine(sum);
